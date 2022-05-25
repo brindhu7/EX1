6ad4657a9cf655784c0418be42be96d42766733c
Brindha <brindha.selvaraj@fau.de>
2022-05-25 11:41:39 +0200

    Forward path

--- exercise1_material/src_to_implement/Optimization/Loss.py
+++ exercise1_material/src_to_implement/Optimization/Loss.py
@@ -4,9 +4,13 @@ class CrossEntropyLoss:
         pass
 
     def forward(self,prediction_tensor,label_tensor):
+        #getting the index of the "1" in one hot encoded label_tensor
+        index = np.where(label_tensor==1)
+        #get the elements of these index in softmax output tensor(prediction_tensor)
+        self.prediction_tensor = prediction_tensor[index]
         epsilon = np.finfo(float).eps
-        error_matrix = -np.sum(label_tensor * np.log(prediction_tensor + epsilon))
-        return error_matrix
+        loss_error_matrix = np.sum(-np.log(self.prediction_tensor + epsilon))
+        return loss_error_matrix
 
     def backward(self,label_tensor):
         return error_tensor
