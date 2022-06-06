2c24402424b604fb4fca2f1d46703470c5597479
Brindha <brindha.selvaraj@fau.de>
2022-06-06 22:53:08 +0200

    Refactored for initializer

--- exercise1_material/src_to_implement/NeuralNetwork.py
+++ exercise1_material/src_to_implement/NeuralNetwork.py
@@ -1,20 +1,25 @@
 import copy
 
 class NeuralNetwork:
-    def __init__(self,Optimizer):
+    def __init__(self,Optimizer,weights_initializer,bias_initializer):
         self.Optimizer = Optimizer
         self.loss = []
         self.layers = []
         self.data_layer = None
         self.loss_layer = None
+        self.weights_initializer = weights_initializer
+        self.bias_initializer = bias_initializer
+
 
     def forward(self):
         input_tensor,label_tensor = self.data_layer.next()
         self.input_tensor = input_tensor
         self.label_tensor  = label_tensor
         #calling forward path for all the layers and return loss in the end
+        #update the input tensor everytime the forward is called
         for i in self.layers:
             self.input_tensor = i.forward(self.input_tensor)
+        #after iterating through all the layers
         return self.loss_layer.forward(self.input_tensor, self.label_tensor)
 
     def backward(self):
@@ -28,7 +33,10 @@ class NeuralNetwork:
         #creating layers in the list layer
         if layer.trainable:
             layer.optimizer = copy.deepcopy(self.Optimizer)
+            #call initialize method for the layer object
+            layer.initialize(self.weights_initializer,self.bias_initializer)
         self.layers.append(layer)
+
 
     def train(self,iterations):
         #calling forward and backward for each iterations
@@ -38,6 +46,7 @@ class NeuralNetwork:
 
     def test(self,input_tensor):
         #input tensor for every layer is the output of the previous tensor
+        #so update input tensor every time forward is called
         for i in self.layers:
             input_tensor= i.forward(input_tensor)
         return input_tensor
