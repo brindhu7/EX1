0821e8e6fa7df2bab06b8a894cd4603730c9ad31
Brindha <brindha.selvaraj@fau.de>
2022-06-21 22:10:06 +0200

    backward remaining

--- exercise1_material/src_to_implement/Layers/Pooling.py
+++ exercise1_material/src_to_implement/Layers/Pooling.py
@@ -1,32 +1,53 @@
 import numpy as np
-from Layers.Base import BaseLayer
+from exercise1_material.src_to_implement.Layers.Base import BaseLayer
+
+
 
 class Pooling(BaseLayer):
     def __init__(self, stride_shape, pooling_shape):
+
         self.stride_shape = stride_shape
         self.pooling_shape = pooling_shape
+        super().__init__()
 
-    def forward(self, input_tensor):
-        print("Hi")
-        count_k = 0
-        count_l = 0
-        A = np.zeros([input_tensor.shape[0], input_tensor.shape[1], self.pooling_shape[0], self.pooling_shape[1]])
-        for i in range(input_tensor.shape[0]):
-           for j in range(input_tensor.shape[1]):
-               count_k = 0
-               for k in range(0, input_tensor.shape[2], self.stride_shape[0]):
-                   count_k = count_k + 1
-                   count_l = 0
-                   for l in range(0, input_tensor.shape[3], self.stride_shape[1]):
-                       count_l = count_l + 1
-                       n_left = l
-                       n_right = l + self.stride_shape[1]
-                       n_top = k
-                       n_down = k + self.stride_shape[0]
-                       A[i, j, count_k, count_l] = np.max(input_tensor[i, j, n_left:n_right, n_top:n_down])
-        return A
-        #pass
 
-    def backward(self, error_tensor):
-        print("Hi")
-        pass
+    def forward(self,input_tensor):
+        self.input_tensor = input_tensor
+        batch,channel,height,width = input_tensor.shape
+        out_h = (height - self.pooling_shape[0]) // self.stride_shape[0] + 1
+        out_w = (width - self.pooling_shape[1]) // self.stride_shape[1] + 1
+        output = np.zeros(shape=(batch,channel,out_h,out_w))
+
+        for i in range(batch):
+            for c in range(channel):
+                for h in range(out_h):
+                    for w in range(out_w):
+                        output[i,c,h,w] = np.max(self.input_tensor[i,c,
+                                                 h*self.stride_shape[0]:h*self.stride_shape[0]+self.pooling_shape[0],
+                                                 w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]])
+
+        return output
+
+
+
+    def backward(self,error_tensor):
+        B,C, H, W = error_tensor.shape
+        dx = np.zeros(self.input_tensor.shape)
+
+        for b in range(B):
+            for c in range(C):
+                for h in range(H):
+                    for w in range(W):
+                        index_h,index_w=np.where(np.max(
+                            self.input_tensor[b,c,h*self.stride_shape[0]:h*self.stride_shape[0]+self.pooling_shape[0],
+                            w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]]) ==
+                                                 self.input_tensor[b,c,h*self.stride_shape[0]:h*self.stride_shape[0]+
+                                                                                              self.pooling_shape[0],
+                                                 w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]])
+                        index_h,index_w = index_h[0],index_w[0]
+                        dx[b,c,h*self.stride_shape[0]:h*self.stride_shape[0]+self.pooling_shape[0],
+                        w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]][index_h,index_w] = \
+                            error_tensor[b,c,h,w]
+        return dx
+
+
