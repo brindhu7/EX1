ba72e04c82a9269bf88bdb2da68d6e5e83217d0e
Brindha <brindha.selvaraj@fau.de>
2022-06-22 02:30:00 +0200

    done

--- exercise1_material/src_to_implement/Layers/Pooling.py
+++ exercise1_material/src_to_implement/Layers/Pooling.py
@@ -5,7 +5,6 @@ from exercise1_material.src_to_implement.Layers.Base import BaseLayer
 
 class Pooling(BaseLayer):
     def __init__(self, stride_shape, pooling_shape):
-
         self.stride_shape = stride_shape
         self.pooling_shape = pooling_shape
         super().__init__()
@@ -14,14 +13,17 @@ class Pooling(BaseLayer):
     def forward(self,input_tensor):
         self.input_tensor = input_tensor
         batch,channel,height,width = input_tensor.shape
+        #defining the output shape
         out_h = (height - self.pooling_shape[0]) // self.stride_shape[0] + 1
         out_w = (width - self.pooling_shape[1]) // self.stride_shape[1] + 1
         output = np.zeros(shape=(batch,channel,out_h,out_w))
-
+        #iterate through the images
         for i in range(batch):
-            for c in range(channel):
-                for h in range(out_h):
+            for c in range(channel):  #iterate through each channel
+                for h in range(out_h): # iterate through the spatial dimensions height and width
                     for w in range(out_w):
+                        #downsampling the input matrix through max pooling
+                        #output the max element for current slice ,sliding over the entire tensor
                         output[i,c,h,w] = np.max(self.input_tensor[i,c,
                                                  h*self.stride_shape[0]:h*self.stride_shape[0]+self.pooling_shape[0],
                                                  w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]])
@@ -38,16 +40,15 @@ class Pooling(BaseLayer):
             for c in range(C):
                 for h in range(H):
                     for w in range(W):
-                        index_h,index_w=np.where(np.max(
-                            self.input_tensor[b,c,h*self.stride_shape[0]:h*self.stride_shape[0]+self.pooling_shape[0],
-                            w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]]) ==
-                                                 self.input_tensor[b,c,h*self.stride_shape[0]:h*self.stride_shape[0]+
-                                                                                              self.pooling_shape[0],
-                                                 w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]])
-                        index_h,index_w = index_h[0],index_w[0]
+                        #current slice window
+                        tmp = self.input_tensor[b,c,h*self.stride_shape[0]:h*self.stride_shape[0]+self.pooling_shape[0],
+                              w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]]
+                        #returning array(mask) of same dimension as window with 1 in max entry
+                        mask = (tmp==np.max(tmp))
                         dx[b,c,h*self.stride_shape[0]:h*self.stride_shape[0]+self.pooling_shape[0],
-                        w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]][index_h,index_w] = \
-                            error_tensor[b,c,h,w]
+                        w*self.stride_shape[1]:w*self.stride_shape[1]+self.pooling_shape[1]] += \
+                            error_tensor[b,c,h,w] * mask
+
         return dx
 
 
--- exercise1_material/src_to_implement/Optimization/Loss.py
+++ exercise1_material/src_to_implement/Optimization/Loss.py
@@ -7,19 +7,19 @@ class CrossEntropyLoss:
     def forward(self, prediction_tensor, label_tensor):
         # getting the prediction tensor for error tensor calculation in backward path
         self.prediction_tensor = prediction_tensor
-        '''
+
         ##getting the index of the "1" in one hot encoded label_tensor
         index = np.where(label_tensor == 1)
         # get the elements of these index in softmax output tensor(prediction_tensor)
         act_layer_op = prediction_tensor[index]
         # Loss for all the entire batch input
-        # loss = np.sum(-np.log(act_layer_op + self.epsilon))
-        '''
-        loss = -np.sum(label_tensor * np.log(prediction_tensor + self.epsilon))
+        loss = np.sum(-np.log(act_layer_op + self.epsilon))
+
+        #loss = -np.sum(label_tensor * np.log(prediction_tensor + self.epsilon))
         return loss
 
     def backward(self, label_tensor):
-        error_tensor = -label_tensor / (self.prediction_tensor + self.epsilon)
+        error_tensor = -label_tensor / (self.prediction_tensor)
         return error_tensor
 
 
